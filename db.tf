c6b952cfa0a580da5bc1af2c65a8360841ab9713
Digo <digomattar@gmail.com>
2024-03-01 08:22:12 -0300

    feat: add ig and route tables

--- db.tf
+++ db.tf
@@ -5,14 +5,15 @@ resource "aws_security_group" "mysql_sg" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = [aws_security_group.wordpress_sg.id]
+    security_groups = [aws_security_group.wordpress_sg.id]
+
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [aws_security_group.wordpress_sg.id]
+    security_groups = [aws_security_group.wordpress_sg.id]
   }
 
   egress {
@@ -31,21 +32,22 @@ resource "aws_instance" "mysql" {
   ami           = "ami-0cf1810907a781f00" #Ubuntu 22.04 us-east-1
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.private_subnet.id
-  security_groups = [aws_security_group.mysql_sg.name]
+  security_groups = [aws_security_group.mysql_sg.id]
   key_name = aws_key_pair.db.key_name
 
-  user_data = <<-EOF
+ user_data = <<-EOF
               #!/bin/bash
               sudo apt update
               sudo apt install -y mysql-server
-              #Secure MySQL installation (this is a placeholder, you'll need to customize this part)
               sudo mysql_secure_installation
-              #Create your database and user
+              # Create your database and user
               sudo mysql -e "CREATE DATABASE wordpress;"
-              sudo mysql -e "CREATE USER '${db_user}'@'%' IDENTIFIED BY '${db_password}';"
-              sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO '${db_user}'@'%';"
+              sudo mysql -e "CREATE USER '${var.db_user}'@'%' IDENTIFIED BY '${var.db_password}';"
+              sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO '${var.db_user}'@'%';"
               sudo mysql -e "FLUSH PRIVILEGES;"
               EOF
+
+
   tags = {
     Name = "MySQL"
   }
--- wordpress.tf
+++ wordpress.tf
@@ -32,41 +32,41 @@ resource "aws_instance" "wordpress" {
   ami           = "ami-0cf1810907a781f00" # Ubuntu 22.04 us-east-1
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
-  security_groups = [aws_security_group.wordpress_sg.name]
+  security_groups = [aws_security_group.wordpress_sg.id]
   key_name = aws_key_pair.wordpress.key_name
   
   user_data = <<-EOF
             #!/bin/bash
             sudo apt update
             sudo apt install -y apache2 php php-curl php-gd php-mbstring php-xml phpxmlrpc php-soap php-intl php-zip php-mysql
-            echo "<VirtualHost *:80>
-                ServerAdmin webmaster@abcplace.com
-                ServerName abcplace.com
-                ServerAlias www.abcplace.com
-                DocumentRoot /var/www/html
-                ErrorLog ${APACHE_LOG_DIR}/error.log
-                CustomLog ${APACHE_LOG_DIR}/access.log combined
-                </VirtualHost>" | sudo tee /etc/apache2/sites-available/abcplace.conf
+            #echo "<VirtualHost *:80>
+             #   ServerAdmin webmaster@abcplace.com
+             #   ServerName abcplace.com
+             #   ServerAlias www.abcplace.com
+             #   DocumentRoot /var/www/html
+             #   ErrorLog  /etc/error.log
+             #   CustomLog /etc/access.log combined
+             #   </VirtualHost>" | sudo tee /etc/apache2/sites-available/abcplace.conf
                 
-              sudo a2ensite abcplace.conf
-              sudo a2dissite 000-default.conf
-              sudo systemctl restart apache2
+              #sudo a2ensite abcplace.conf
+              #sudo a2dissite 000-default.conf
+              #sudo systemctl restart apache2
               
               # Install Certbot and request an SSL certificate for ABC Place
-              sudo apt install -y software-properties-common
-              sudo add-apt-repository universe
-              sudo add-apt-repository ppa:certbot/certbot
-              sudo apt update
-              sudo apt install -y certbot python3-certbot-apache
-              sudo certbot --apache --domains abcplace.com,www.abcplace.com --non-interactive --agree-tos
+              #sudo apt install -y software-properties-common
+              #sudo add-apt-repository universe
+              #sudo add-apt-repository ppa:certbot/certbot
+              #sudo apt update
+              #sudo apt install -y certbot python3-certbot-apache
+              #sudo certbot --apache --domains abcplace.com,www.abcplace.com --non-interactive --agree-tos
 
-              cd /var/www/html
-              sudo wget https://wordpress.org/latest.tar.gz
-              sudo tar -xzf latest.tar.gz
-              sudo cp -r wordpress/* /var/www/html/
-              sudo rm -rf wordpress latest.tar.gz
-              sudo chown -R www-data:www-data /var/www/html
-              sudo systemctl restart apache2
+              #cd /var/www/html
+              #sudo wget https://wordpress.org/latest.tar.gz
+              #sudo tar -xzf latest.tar.gz
+              #sudo cp -r wordpress/* /var/www/html/
+              #sudo rm -rf wordpress latest.tar.gz
+              #sudo chown -R www-data:www-data /var/www/html
+              #sudo systemctl restart apache2
               EOF
 
   tags = {
